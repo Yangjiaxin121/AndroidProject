b0c264f29358c621a0532059e515c06422c7759f
Yangjiaxin121 <1752346954@qq.com>
2018-08-02 13:36:14 +0800

    口袋校园

--- PocketCampus2-master/PocketCampus2/app/src/main/java/com/example/yangjiaxin/pocketcampus/Fragment3.java
+++ PocketCampus2-master/PocketCampus2/app/src/main/java/com/example/yangjiaxin/pocketcampus/Fragment3.java
@@ -1,83 +1,44 @@
 package com.example.yangjiaxin.pocketcampus;
 
 
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.widget.GridLayoutManager;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.example.yangjiaxin.pocketcampus.recyclerview.Fruit;
-import com.example.yangjiaxin.pocketcampus.recyclerview.FruitAdapter;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Random;
-
 /**
  * Created by IT-CTY on 2018/4/25.
  */
 
 public class Fragment3 extends Fragment {
-
-
-    private Fruit[] fruits = {new Fruit("Apple", R.drawable.apple), new Fruit("Banana", R.drawable.banana),
-            new Fruit("Orange", R.drawable.orange), new Fruit("Watermelon", R.drawable.watermelon),
-            new Fruit("Pear", R.drawable.pear), new Fruit("Grape", R.drawable.grape),
-            new Fruit("Pineapple", R.drawable.pineapple), new Fruit("Strawberry", R.drawable.strawberry),
-            new Fruit("Cherry", R.drawable.cherry), new Fruit("Mango", R.drawable.mango)};
-
-    private List<Fruit> fruitList = new ArrayList<>();
-
-    private FruitAdapter adapter;
-
-
-    private Context mContext;
-
-
+    private TextView textView;
+    private Button button;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view=inflater.inflate(R.layout.fragment_fragment3,container,false);
-        mContext = this.getActivity();
-        initFruits();
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recycler_view1);
-        GridLayoutManager layoutManager = new GridLayoutManager(mContext, 2);
-        recyclerView.setLayoutManager(layoutManager);
-        adapter = new FruitAdapter(fruitList);
-        recyclerView.setAdapter(adapter);
         return view;
     }
 
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        textView=(TextView)getActivity().findViewById(R.id.textView3);
+        button=(Button)getActivity().findViewById(R.id.button3);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getActivity(),"Fragment2",Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
-
-    private void initFruits() {
-        fruitList.clear();
-        for (int i = 0; i < 50; i++) {
-            Random random = new Random();
-            int index = random.nextInt(fruits.length);
-            fruitList.add(fruits[index]);
-        }
     }
-
-
 }
+
